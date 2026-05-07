8e5a028d221e27a761629f9c2988c64b26703e8d
Stefan <stefan.manolescu99@yahoo.com>
2026-05-07 10:26:18 +0300

    The system reacts too slow for PID to be a viable type of controller, a simple on-off controller is enough

--- autoclave/autoclave.ino
+++ autoclave/autoclave.ino
@@ -3,7 +3,6 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27,16,2);  //sometimes the adress is not 0x27. Change to 0x3f if it dosn't work.
 
-
 //Inputs and outputs
 int firing_pin = 3;
 int zero_cross = 8;
@@ -14,114 +13,38 @@ int thermoCLK = 12;
 //Start a MAX6675 communication with the selected pins
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
-
-//Variables
-int last_CH1_state = 0;
-bool zero_cross_detected = false;
-int firing_delay = 8000;
-
-//////////////////////////////////////////////////////
-int maximum_firing_delay = 3000;
-/*Later in the code you will se that the maximum delay after the zero detection
- * is 7400. Why? Well, we know that the 220V AC voltage has a frequency of around 50-60HZ so
- * the period is between 20ms and 16ms, depending on the country. We control the firing
- * delay each half period so each 10ms or 8 ms. To amke sure we wont pass thsoe 10ms, I've made tests
- * and the 7400us or 7.4ms was a good value. Measure your frequency and chande that value later */
-//////////////////////////////////////////////////////
-
-unsigned long previousMillis = 0; 
-unsigned long currentMillis = 0;
-int temp_read_Delay = 3000;
 int real_temperature = 0;
 int setpoint = 111;
-bool pressed_1 = false;
-bool pressed_2 = false;
-int zero_crosscount=0;
-
-//PID variables
-float PID_error = 0;
-float previous_error = 0;
-float elapsedTime, Time, timePrev;
-int PID_value = 0;
-//PID constants
-int kp = 300;   int ki= 10;   int kd = 1.04;
-int PID_p = 0;    int PID_i = 0;    int PID_d = 0;
-
-
-
-
-
 
 void setup() {
-  //Define the pins
   pinMode (firing_pin,OUTPUT); 
-  pinMode (zero_cross,INPUT);  
-  PCICR |= (1 << PCIE0);    //enable PCMSK0 scan                                                 
-  PCMSK0 |= (1 << PCINT0);  //Set pin D8 (zero cross input) trigger an interrupt on state change.
-  pinMode(3,OUTPUT);        //Define D3 as output for the DIAC pulse
   Serial.begin(9600);       //Start serial com with the BT module (RX and TX pins   
   lcd.init();       //Start the LC communication
   lcd.backlight();  //Turn on backlight for LCD
 }
 
-
 void loop() {    
-  currentMillis = millis();           //Save the value of time before the loop
-   /*  We create this if so we will read the temperature and change values each "temp_read_Delay"
-    *  value. Change that value above iv you want. The MAX6675 read is slow. Tha will affect the
-    *  PID control. I've tried reading the temp each 100ms but it didn't work. With 500ms worked ok.*/
-  if(currentMillis - previousMillis >= temp_read_Delay){
-    previousMillis += temp_read_Delay;              //Increase the previous time for next loop
-    real_temperature = thermocouple.readCelsius();  //get the real temperature in Celsius degrees
+               //Increase the previous time for next loop
+  real_temperature = thermocouple.readCelsius();  //get the real temperature in Celsius degrees
+  if (real_temperature>=setpoint)
+    digitalWrite(firing_pin,LOW);
+  else
+    digitalWrite(firing_pin,HIGH);
+  delay(3000);
+  
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Set: ");
+  lcd.setCursor(5,0);
+  lcd.print(setpoint);
+  lcd.setCursor(0,1);
+  lcd.print("Real temp: ");
+  lcd.setCursor(11,1);
+  lcd.print(real_temperature);
 
-    PID_error = setpoint - real_temperature;        //Calculate the pid ERROR
-    
-    if(PID_error > 10)                              //integral constant will only affect errors below 10ºC to avoid actuator windup            
-    {PID_i = 0;}
-    
-    PID_p = kp * PID_error;                         //Calculate the P value
-    PID_i = PID_i + (ki * PID_error);               //Calculate the I value
-    timePrev = Time;                    // the previous time is stored before the actual time read
-    Time = millis();                    // actual time read
-    elapsedTime = (Time - timePrev) / 1000;   
-    PID_d = kd*((PID_error - previous_error)/elapsedTime);  //Calculate the D value
-    PID_value = PID_p + PID_i + PID_d;                      //Calculate total PID value
-
-    if(PID_error < -2)            // in case of too much actuator windup deal with it by resetting PID value  
-    {PID_value = 0;
-    PID_i = 0;}
-    //We define firing delay range between 0 and 7400. Read above why 7400!!!!!!!
-    if(PID_value < 0)
-    {      PID_value = 0;       }
-    if(PID_value > maximum_firing_delay)
-    {      PID_value = maximum_firing_delay;    }
-    //Printe the values on the LCD
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Set: ");
-    lcd.setCursor(5,0);
-    lcd.print(setpoint);
-    lcd.setCursor(0,1);
-    lcd.print("Real temp: ");
-    lcd.setCursor(11,1);
-    lcd.print(real_temperature);
-    previous_error = PID_error; //Remember to store the previous error.
-    Serial.println(real_temperature);
-    
-  }
+  Serial.println(real_temperature);
   if(Serial.available()>0)
    { 
       setpoint = Serial.read();
    }
-   
-  //If the zero cross interruption was detected we create the 100us firing pulse  
-  
-      digitalWrite(firing_pin,HIGH);
-      delayMicroseconds(PID_value*1000); //This delay controls the power
-      if(PID_value < maximum_firing_delay){
-        digitalWrite(firing_pin,LOW);
-        delayMicroseconds((maximum_firing_delay - PID_value)*1000);
-      }
-      
-    
 }
