dec644c538f256d860438108192a3f29635ea00e
Stefan <stefan.manolescu99@yahoo.com>
2026-05-07 10:16:17 +0300

    The triac burned out and changed the control electronics to a SSR, minor pid values changes

--- autoclave/autoclave.ino
+++ autoclave/autoclave.ino
@@ -11,7 +11,6 @@
 #include "max6675.h"
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-
 LiquidCrystal_I2C lcd(0x27,16,2);  //sometimes the adress is not 0x27. Change to 0x3f if it dosn't work.
 
 /*    i2c LCD Module  ==>   Arduino
@@ -34,10 +33,10 @@ MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 //Variables
 int last_CH1_state = 0;
 bool zero_cross_detected = false;
-int firing_delay = 7400;
+int firing_delay = 8000;
 
 //////////////////////////////////////////////////////
-int maximum_firing_delay = 7400;
+int maximum_firing_delay = 3000;
 /*Later in the code you will se that the maximum delay after the zero detection
  * is 7400. Why? Well, we know that the 220V AC voltage has a frequency of around 50-60HZ so
  * the period is between 20ms and 16ms, depending on the country. We control the firing
@@ -47,9 +46,9 @@ int maximum_firing_delay = 7400;
 
 unsigned long previousMillis = 0; 
 unsigned long currentMillis = 0;
-int temp_read_Delay = 500;
+int temp_read_Delay = 3000;
 int real_temperature = 0;
-int setpoint = 101;
+int setpoint = 100;
 bool pressed_1 = false;
 bool pressed_2 = false;
 int zero_crosscount=0;
@@ -60,11 +59,8 @@ float previous_error = 0;
 float elapsedTime, Time, timePrev;
 int PID_value = 0;
 //PID constants
-int kp = 203;   int ki= 7.2;   int kd = 1.04;
+int kp = 300;   int ki= 10;   int kd = 1.04;
 int PID_p = 0;    int PID_i = 0;    int PID_d = 0;
-
-
-
 
 
 
@@ -78,7 +74,7 @@ void setup() {
   PCICR |= (1 << PCIE0);    //enable PCMSK0 scan                                                 
   PCMSK0 |= (1 << PCINT0);  //Set pin D8 (zero cross input) trigger an interrupt on state change.
   pinMode(3,OUTPUT);        //Define D3 as output for the DIAC pulse
-  Serial.begin(9600);        //Start serial com with the BT module (RX and TX pins   
+  Serial.begin(9600);       //Start serial com with the BT module (RX and TX pins   
   lcd.init();       //Start the LC communication
   lcd.backlight();  //Turn on backlight for LCD
 }
@@ -95,7 +91,7 @@ void loop() {
 
     PID_error = setpoint - real_temperature;        //Calculate the pid ERROR
     
-    if(PID_error > 5)                              //integral constant will only affect errors below 5ºC             
+    if(PID_error > 10)                              //integral constant will only affect errors below 10ºC to avoid actuator windup            
     {PID_i = 0;}
     
     PID_p = kp * PID_error;                         //Calculate the P value
@@ -106,6 +102,9 @@ void loop() {
     PID_d = kd*((PID_error - previous_error)/elapsedTime);  //Calculate the D value
     PID_value = PID_p + PID_i + PID_d;                      //Calculate total PID value
 
+    if(PID_error < -2)            // in case of too much actuator windup deal with it by resetting PID value  
+    {PID_value = 0;
+    PID_i = 0;}
     //We define firing delay range between 0 and 7400. Read above why 7400!!!!!!!
     if(PID_value < 0)
     {      PID_value = 0;       }
@@ -123,8 +122,8 @@ void loop() {
     lcd.print(real_temperature);
     //lcd.print(zero_crosscount);
     previous_error = PID_error; //Remember to store the previous error.
-    Serial.print(real_temperature);
-    Serial.println(";");
+    Serial.println(real_temperature);
+    
   }
   if(Serial.available()>0)
    { 
@@ -136,15 +135,13 @@ void loop() {
    }
    
   //If the zero cross interruption was detected we create the 100us firing pulse  
-  if (zero_cross_detected)     
-    {
-      delayMicroseconds(maximum_firing_delay - PID_value); //This delay controls the power
+  
       digitalWrite(firing_pin,HIGH);
-      delayMicroseconds(100);
+      delayMicroseconds(PID_value*1000); //This delay controls the power
       digitalWrite(firing_pin,LOW);
-      zero_cross_detected = false;
-      zero_crosscount=zero_crosscount+1;
-    } 
+      delayMicroseconds((maximum_firing_delay - PID_value)*1000);
+      
+    
 }
 //End of void loop
 // |
@@ -176,31 +173,6 @@ ISR(PCINT0_vect){
     last_CH1_state = 0;            //Store the current state into the last state for the next loop
     }
 
-/*    if(PINB & B00001000){          //We make an AND with the state register, We verify if pin D11 is HIGH???
-      if (!pressed_1)
-      {
-        setpoint = setpoint + 5;   //Increase the temperature by 5. Change this with your value if you want.
-        delay(20);
-        pressed_1 = true;
-      }
-    }
-    else if (pressed_1)
-    {
-      pressed_1 = false;
-    }
 
-    if(PINB & B00010000){          //We make an AND with the state register, We verify if pin D12 is HIGH???
-      if (!pressed_2)
-      {
-        setpoint = setpoint - 5;   //Decrease the temperature by 5. Change this with your value if you want.
-        delay(20);
-        pressed_2 = true;
-      }
-    }
-    else if (pressed_2)
-    {
-      pressed_2 = false;
-    }
-}
 //End of interruption vector for pins on port B: D8-D13 */
 }
